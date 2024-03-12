7e33032260bb18fe00d683c2a1a3c01a0398ccff
Armin Ghasemi <armin.ghassemi@gmail.com>
2024-03-12 23:29:53 +0100

    TSK16: improve excel validation tests

--- tests/Feature/ImportManagement/ExcelImportTest.php
+++ tests/Feature/ImportManagement/ExcelImportTest.php
@@ -7,9 +7,9 @@ use App\services\ImportData\Validators\ValidationException;
 use \Maatwebsite\Excel\Facades\Excel;
 use \App\Imports\SimpleImport;
 
-it('can read excel file', function(){
+it('can read excel file', function () {
     $testFilePath = \Pest\testDirectory(
-        str_replace('/',DIRECTORY_SEPARATOR,'Feature/ImportManagement/SampleData/sample_excel_01.xlsx')
+        str_replace('/', DIRECTORY_SEPARATOR, 'Feature/ImportManagement/SampleData/sample_excel_01.xlsx')
     );
 
     $arrayData = Excel::toCollection(new SimpleImport(), $testFilePath);
@@ -18,9 +18,9 @@ it('can read excel file', function(){
     expect($arrayData[0][1][0])->toBe('Flexplek 1');
 });
 
-it('can read excel file with header', function(){
+it('can read excel file with header', function () {
     $testFilePath = \Pest\testDirectory(
-        str_replace('/',DIRECTORY_SEPARATOR,'Feature/ImportManagement/SampleData/sample_excel_01.xlsx')
+        str_replace('/', DIRECTORY_SEPARATOR, 'Feature/ImportManagement/SampleData/sample_excel_01.xlsx')
     );
 
     $importer = new ImportExecutor(new ExcelImportStrategy());
@@ -30,17 +30,28 @@ it('can read excel file with header', function(){
     expect($workstation)->not->toBeEmpty();
 });
 
-it('fails when read excel file with validation issues', function(){
+it('fails when read excel file with validation issues', function () {
     $testFilePath = \Pest\testDirectory(
-        str_replace('/',DIRECTORY_SEPARATOR,'Feature/ImportManagement/SampleData/sample_excel_02_fail.xlsx')
+        str_replace('/', DIRECTORY_SEPARATOR, 'Feature/ImportManagement/SampleData/sample_excel_02_fail.xlsx')
     );
 
-    try{
+    try {
         $importer = new ImportExecutor(new ExcelImportStrategy());
         $importer->execute($testFilePath);
-        expect()->fail();
-    }catch(ValidationException $e){
-        expect($e->getMessage())->toContain('Each workstation must have either a unique name or a user');
+        expect()->fail('No ValidationException was thrown!');
+    } catch (ValidationException $e) {
+        foreach ($e->getErrors() as $error) {
+            switch($error['row_number']) {
+                case 3:
+                    expect($error['message'])
+                        ->toContain('Each workstation must have either a unique name or a user');
+                break;
+                case 4:
+                    expect($error['message'])
+                        ->toContain('User email is required if a user is provided');
+                break;
+            }
+        }
     }
 
 });
