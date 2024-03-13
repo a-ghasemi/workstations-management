bf2e0c98dfa0bd5524f93133c14696eac3fa1cd1
Armin Ghasemi <armin.ghassemi@gmail.com>
2024-03-13 10:21:20 +0100

    TSK17: add bootstrap package

--- resources/views/include/message.blade.php
+++ resources/views/include/message.blade.php
@@ -1,25 +1,13 @@
-<div class="toast-container position-static">
 @if(session('success'))
-    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <strong class="me-auto">Success</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-            {!! session('success') !!}
-        </div>
+    <div class="callout callout-success">
+        <h4>Success</h4>
+        {!! session('success') !!}
     </div>
 @endif
 
 @if(session('error'))
-    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <strong class="me-auto">Error</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-            {!! session('error') !!}
-        </div>
+    <div class="callout callout-danger">
+        <h4>Error</h4>
+        {!! session('error') !!}
     </div>
 @endif
-</div>
